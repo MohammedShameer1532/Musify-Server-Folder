4ce8e3475fc2a0b37fbb42ac096ae554de868f0c
Mohammed Shameer <118889168+MohammedShameer1532@users.noreply.github.com>
2024-12-11 12:28:48 +0530

    Update index.js https

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(session({
     mongoUrl: process.env.DATABASE,
   }),
   cookie: {
-    secure: true, // Set to true in production if using HTTPS
+    secure: false, // Set to true in production if using HTTPS
     maxAge: 24 * 60 * 60 * 1000, // 1 day
   },
 }));
